da0747aa128bcc5475a2b69407f465a6fd304b09
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-07-13 21:24:08 +0000

    Undefined problem when generating excuse fixed.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let whoArray = ["The dog", "My grandma", "His turtle", "My bird"];
+let actionArray = ["ate", "peed", "crushed", "broke"];
+let whatArray = ["my homework", "the keys", "the car"];
+let whenArray = [
+  "before the class.",
+  "right on time.",
+  "when I finished.",
+  "during my lunch.",
+  "while I was praying."
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = whoArray[Math.floor(Math.random() * whoArray.length)];
+  let randomAction =
+    actionArray[Math.floor(Math.random() * actionArray.length)];
+  let randomWhat = whatArray[Math.floor(Math.random() * whatArray.length)];
+  let randomWhen = whenArray[Math.floor(Math.random() * whenArray.length)];
+
+  document.getElementById("excuse").innerHTML =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
 };
